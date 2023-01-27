c35b54ed657956e50575ff025f805c57cc0a8242
adenh93 <aden.herold1@gmail.com>
2023-01-27 20:16:33 +1100

    feat: updated main function to bind TcpListener to port 4000 + run server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use poll_api::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("localhost:4000")?;
+    let server = run(listener)?;
+
+    server.await?;
+
+    Ok(())
 }
