8443f7bb079fc276981f73f9def6601f83e1b877
adenh93 <aden.herold1@gmail.com>
2023-01-28 09:52:34 +1100

    feat: updated main function to create connection_pool and pass to Application constructor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use poll_api::{config::get_config, startup::Application};
+use poll_api::{
+    config::get_config,
+    startup::{get_connection_pool, Application},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_config().expect("Failed to read config");
-    let application = Application::build(config).await?;
+    let connection_pool = get_connection_pool(&config.database);
+    let application = Application::build(config, connection_pool).await?;
 
     application.run_until_stopped().await?;
 
