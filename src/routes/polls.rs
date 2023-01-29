2150aa42f98c51be1f6232632bf90257d24d6335
adenh93 <aden.herold1@gmail.com>
2023-01-29 16:09:18 +1100

    feat: configured instrumentation for get_poll route

--- src/routes/polls.rs
+++ src/routes/polls.rs
@@ -2,7 +2,8 @@ use actix_web::{get, web, HttpResponse};
 use sqlx::PgPool;
 use uuid::Uuid;
 
+#[tracing::instrument(name = "Fetching a poll", skip(conn))]
 #[get("/polls/{id}")]
-pub async fn get_poll(_id: web::Path<Uuid>, _conn: web::Data<PgPool>) -> HttpResponse {
+pub async fn get_poll(id: web::Path<Uuid>, conn: web::Data<PgPool>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
