61f17ef32599fd7bba8b0170b86eade9c008cbc5
adenh93 <aden.herold1@gmail.com>
2023-01-29 20:34:30 +1100

    feat: added Poll domain model

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,3 +1,11 @@
-pub mod created_poll;
-pub mod new_poll;
-pub mod new_poll_choice;
+mod created_poll;
+mod new_poll;
+mod new_poll_choice;
+mod poll;
+mod poll_choice;
+
+pub use created_poll::CreatedPoll;
+pub use new_poll::NewPoll;
+pub use new_poll_choice::NewPollChoice;
+pub use poll::Poll;
+pub use poll_choice::PollChoice;
---
+++ src/domain/poll.rs
@@ -0,0 +1,15 @@
+use super::PollChoice;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+pub struct Poll {
+    pub id: Uuid,
+    pub name: String,
+    pub description: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub end_date: DateTime<Utc>,
+    pub choices: Vec<PollChoice>,
+}
