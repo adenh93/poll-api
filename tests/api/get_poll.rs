1fd40c788aafb3f7e7cb81b483e11e1ad705a9ba
adenh93 <aden.herold1@gmail.com>
2023-01-29 20:38:43 +1100

    test: added integration tests for get_poll route

--- tests/api/get_poll.rs
+++ tests/api/get_poll.rs
@@ -1,4 +1,5 @@
-use crate::helpers::TestApp;
+use crate::helpers::{generate_poll, TestApp};
+use poll_api::domain::{CreatedPoll, Poll};
 use uuid::Uuid;
 
 #[tokio::test]
@@ -8,4 +9,19 @@ async fn fails_to_get_non_existent_poll() {
 
     let response = app.get_poll(&uuid).await;
     assert_eq!(response.status().as_u16(), 404);
+}
+
+#[tokio::test]
+async fn gets_existing_election() {
+    let app = TestApp::new().await;
+
+    let new_poll = generate_poll(3);
+    let response = app.post_poll(&new_poll).await;
+    let created_poll = response.json::<CreatedPoll>().await.unwrap();
+
+    let response = app.get_poll(&created_poll.id).await;
+    assert!(response.status().is_success());
+
+    let poll = response.json::<Poll>().await.unwrap();
+    assert_eq!(poll.id, created_poll.id);
 }
