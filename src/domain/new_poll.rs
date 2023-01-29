62659fd722be46bd9e667805d40e952151c45f30
adenh93 <aden.herold1@gmail.com>
2023-01-29 19:00:05 +1100

    feat: added custom validator to new_poll to validate end_date of poll

--- src/domain/new_poll.rs
+++ src/domain/new_poll.rs
@@ -1,12 +1,46 @@
 use super::new_poll_choice::NewPollChoice;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use validator::{Validate, ValidationError};
 
 #[derive(Debug, Validate, Serialize, Deserialize)]
 pub struct NewPoll {
     pub name: String,
+    #[validate(custom = "end_date_greater_than_utc_now")]
     pub end_date: DateTime<Utc>,
     #[validate(length(min = 2))]
     pub choices: Vec<NewPollChoice>,
+}
+
+fn end_date_greater_than_utc_now(end_date: &DateTime<Utc>) -> Result<(), ValidationError> {
+    if *end_date < Utc::now() {
+        return Err(ValidationError::new("End date must be in the future"));
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fake::faker::chrono::en::DateTimeBefore;
+    use fake::faker::lorem::en::Sentence;
+    use fake::Fake;
+
+    #[test]
+    fn fails_to_validate_if_end_date_in_past() {
+        let name = Sentence(5..8);
+
+        let choices = (0..3)
+            .map(|_| NewPollChoice { name: name.fake() })
+            .collect();
+
+        let new_poll = NewPoll {
+            name: name.fake(),
+            end_date: DateTimeBefore(Utc::now()).fake(),
+            choices,
+        };
+
+        assert!(new_poll.validate().is_err());
+    }
 }
