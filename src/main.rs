aadcfffa744b0da08833c019ac62ffbc124636cd
adenh93 <aden.herold1@gmail.com>
2023-01-29 16:08:44 +1100

    feat: registered tracing subscriber in main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use poll_api::{
     config::get_config,
     startup::{get_connection_pool, Application},
+    telemetry::{get_subscriber, init_subscriber},
 };
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("poll-api", "info", std::io::stdout);
+    init_subscriber(subscriber);
+
     let config = get_config().expect("Failed to read config");
     let connection_pool = get_connection_pool(&config.database);
     let application = Application::build(config, connection_pool).await?;
