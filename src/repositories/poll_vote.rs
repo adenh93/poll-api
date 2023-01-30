54b53c3d4307dbc05aa5b751a8f60762f98ed1c2
adenh93 <aden.herold1@gmail.com>
2023-01-30 21:24:46 +1100

    feat: added insert_poll_vote function to poll_vote repository

--- src/repositories/poll_vote.rs
+++ src/repositories/poll_vote.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use sqlx::{types::ipnetwork::IpNetwork, PgPool};
 use uuid::Uuid;
 
@@ -5,10 +6,10 @@ use crate::domain::PollVote;
 
 #[tracing::instrument(
     name = "Fetch poll vote by IP Address",
-    skip(poll_uuid, ip_address, conn)
+    skip(poll_id, ip_address, conn)
 )]
 pub async fn get_poll_vote_by_ip_address(
-    poll_uuid: &Uuid,
+    poll_id: &Uuid,
     ip_address: &IpNetwork,
     conn: &PgPool,
 ) -> sqlx::Result<Option<PollVote>> {
@@ -19,7 +20,7 @@ pub async fn get_poll_vote_by_ip_address(
         FROM poll_vote
         WHERE poll_id = $1 AND ip_address = $2
         "#,
-        poll_uuid,
+        poll_id,
         ip_address,
     )
     .fetch_optional(conn)
@@ -30,4 +31,37 @@ pub async fn get_poll_vote_by_ip_address(
     })?;
 
     Ok(vote)
+}
+
+#[tracing::instrument(
+    name = "Inserting poll vote record",
+    skip(poll_id, choice_id, ip_address, conn)
+)]
+pub async fn insert_poll_vote(
+    poll_id: &Uuid,
+    choice_id: &Uuid,
+    ip_address: &IpNetwork,
+    conn: &PgPool,
+) -> sqlx::Result<()> {
+    let uuid = Uuid::new_v4();
+
+    sqlx::query!(
+        r#"
+        INSERT INTO poll_vote (id, poll_id, choice_id, ip_address, created_at)
+        VALUES ($1, $2, $3, $4, $5)
+        "#,
+        uuid,
+        poll_id,
+        choice_id,
+        ip_address,
+        Utc::now(),
+    )
+    .execute(conn)
+    .await
+    .map_err(|err| {
+        tracing::error!("Failed to execute query: {err:?}");
+        err
+    })?;
+
+    Ok(())
 }
