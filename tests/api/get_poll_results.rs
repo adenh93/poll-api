f5d02d8c9be33465f05637177c5d09a887229d5b
adenh93 <aden.herold1@gmail.com>
2023-01-31 19:44:51 +1100

    test: added integration tests for get_poll_results route

---
+++ tests/api/get_poll_results.rs
@@ -0,0 +1,66 @@
+use crate::helpers::{generate_poll, TestApp};
+use poll_api::domain::{Poll, PollResults};
+
+#[tokio::test]
+async fn gets_poll_results() {
+    const VOTE_COUNT: usize = 100;
+
+    let app = TestApp::new().await;
+
+    let generated_poll = generate_poll(5, true);
+    let uuid = app.add_past_election(&generated_poll).await.unwrap().id;
+
+    let response = app.get_poll(&uuid).await;
+    let poll = response.json::<Poll>().await.unwrap();
+
+    app.simulate_poll(&poll, VOTE_COUNT).await.unwrap();
+
+    let response = app.get_poll_results(&uuid).await;
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn poll_results_add_up_to_vote_count() {
+    const VOTE_COUNT: usize = 100;
+
+    let app = TestApp::new().await;
+
+    let generated_poll = generate_poll(5, true);
+    let uuid = app.add_past_election(&generated_poll).await.unwrap().id;
+
+    let response = app.get_poll(&uuid).await;
+    let poll = response.json::<Poll>().await.unwrap();
+
+    app.simulate_poll(&poll, VOTE_COUNT).await.unwrap();
+
+    let response = app.get_poll_results(&uuid).await;
+    let results = response.json::<Vec<PollResults>>().await.unwrap();
+
+    let vote_count: usize = results
+        .iter()
+        .fold(0, |acc, next| acc + next.vote_count.unwrap() as usize);
+
+    assert_eq!(vote_count, VOTE_COUNT);
+}
+
+#[tokio::test]
+async fn poll_results_are_in_desc_order_by_vote_count() {
+    const VOTE_COUNT: usize = 1_000;
+
+    let app = TestApp::new().await;
+
+    let generated_poll = generate_poll(5, true);
+    let uuid = app.add_past_election(&generated_poll).await.unwrap().id;
+
+    let response = app.get_poll(&uuid).await;
+    let poll = response.json::<Poll>().await.unwrap();
+
+    app.simulate_poll(&poll, VOTE_COUNT).await.unwrap();
+
+    let response = app.get_poll_results(&uuid).await;
+    let results = response.json::<Vec<PollResults>>().await.unwrap();
+    let vote_counts: Vec<i64> = results.iter().map(|res| res.vote_count.unwrap()).collect();
+    let is_in_desc_order = vote_counts.iter().is_sorted_by(|a, b| b.partial_cmp(a));
+
+    assert!(is_in_desc_order);
+}
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,6 +1,8 @@
+#![feature(is_sorted)]
+
 mod create_poll;
 mod get_poll;
+mod get_poll_results;
 mod health_check;
 mod helpers;
 mod vote_poll;
-
