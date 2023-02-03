855d0585276a6bbf8265f81f2641713909853554
adenh93 <aden.herold1@gmail.com>
2023-02-03 21:20:43 +1100

    feat: implemented error_response method for HttpError HttpResponse impl
    feat: added pub name method to HttpError impl to return string representation of error variants

--- src/errors/http_error.rs
+++ src/errors/http_error.rs
@@ -1,5 +1,6 @@
 use super::error_chain_fmt;
-use actix_web::{http::StatusCode, ResponseError};
+use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use serde::Serialize;
 
 pub type HttpResult<T> = Result<T, HttpError>;
 
@@ -15,6 +16,17 @@ pub enum HttpError {
     UnexpectedError(#[from] anyhow::Error),
 }
 
+impl HttpError {
+    pub fn name(&self) -> String {
+        match self {
+            Self::ValidationError(_) => "ValidationError".into(),
+            Self::UserError(_) => "UserError".into(),
+            Self::NotFoundError(_) => "NotFoundError".into(),
+            Self::UnexpectedError(_) => "UnexpectedError".into(),
+        }
+    }
+}
+
 impl std::fmt::Debug for HttpError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
@@ -24,10 +36,29 @@ impl std::fmt::Debug for HttpError {
 impl ResponseError for HttpError {
     fn status_code(&self) -> StatusCode {
         match self {
-            HttpError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            HttpError::UserError(_) => StatusCode::BAD_REQUEST,
-            HttpError::NotFoundError(_) => StatusCode::NOT_FOUND,
-            HttpError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::ValidationError(_) => StatusCode::BAD_REQUEST,
+            Self::UserError(_) => StatusCode::BAD_REQUEST,
+            Self::NotFoundError(_) => StatusCode::NOT_FOUND,
+            Self::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code()).json(ErrorResponseBody::from(self))
+    }
+}
+
+#[derive(Serialize)]
+pub struct ErrorResponseBody {
+    pub name: String,
+    pub message: String,
+}
+
+impl From<&HttpError> for ErrorResponseBody {
+    fn from(err: &HttpError) -> Self {
+        Self {
+            name: err.name(),
+            message: err.to_string(),
         }
     }
 }
